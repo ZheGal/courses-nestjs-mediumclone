e13b4eb8e6039dc5bef715f6c3852eb0c4fb6539
Evgeniy Galkovskiy <zhegal@gmail.com>
2022-05-04 22:14:04 +0300

    article remove likes

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -123,7 +123,7 @@ export class ArticleService {
         slug: string,
         currentUserId: number,
     ): Promise<ArticleEntity> {
-        const article = await this.getArticleBySlug(slug)
+        const article = await this.getArticleBySlug(slug);
         const user = await this.userRepository.findOne(currentUserId, {
             relations: ['favorites']
         });
@@ -134,6 +134,28 @@ export class ArticleService {
         if (isNotFavorited) {
             user.favorites.push(article);
             article.favouritesCount++;
+            await this.userRepository.save(user);
+            await this.articleRepository.save(article);
+        }
+
+        return article;
+    }
+
+    async deleteArticleFromFavorites(
+        slug: string,
+        currentUserId: number,
+    ) {
+        const article = await this.getArticleBySlug(slug);
+        const user = await this.userRepository.findOne(currentUserId, {
+            relations: ['favorites']
+        });
+        const articleIndex = user.favorites.findIndex(
+            (articleInFavorites) => articleInFavorites.id === article.id
+        );
+
+        if (articleIndex >= 0) {
+            user.favorites.splice(articleIndex, 1);
+            article.favouritesCount--;
             await this.userRepository.save(user);
             await this.articleRepository.save(article);
         }
